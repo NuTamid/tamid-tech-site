247fb9eb8210d33531f62904d6ed4eb56615b395
Kris Amerman <amerman.k@northeastern.edu>
2023-06-26 16:31:50 -0400

    initialize branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,7 @@ const HomePage = () => {
   return (
     <main>
       <div className="bg-black">
-        <h3 className="text-white">The next era of tech @ TAMID Northeastern</h3>
-        <h1 className="text-white">TAMID Tech</h1>
-        <div className="text-white">Image placeholder!</div>
+        
       </div>
     </main>
   )
